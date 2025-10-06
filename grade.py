67d57c381edf8f4ff5f3c4827d581dcc70f385b5
AishwaryaPM11 <aishwaryamulagund0@gmail.com>
2025-10-06 14:43:50 +0530

    save

--- grade.py
+++ grade.py
@@ -12,3 +12,27 @@ elif mark >= 60:
 else:
     grade = 'Fail'
 print(f"Grade: {grade}")
+def get_grade(average):
+    if average >= 90:
+        return 'A'
+    elif average >= 80:
+        return 'B'
+    elif average >= 70:
+        return 'C'
+    elif average >= 60:
+        return 'D'
+    else:
+        return 'Fail'
+
+if _name_ == "_main_":
+    marks = []
+
+    for i in range(1, 6):
+        mark = float(input(f"Enter marks for student {i}: "))
+        marks.append(mark)
+
+    average = compute_average(marks)
+    grade = get_grade(average)
+
+    print(f"\nAverage Marks: {average:.2f}")
+    print(f"Grade: {grade}")
